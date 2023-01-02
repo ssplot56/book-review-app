19c1ce0ecbcc22036e8443d16776beacde4d5be4
ssplot56 <ssplot56@gmail.com>
2023-01-02 19:22:38 +0200

    initial commit

---
+++ src/test/java/com/splot/app/BookReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
